4396d91b6aa83b165d343762919c13dcb1ba4eeb
Nicholas White <nkwkona@gmail.com>
2022-02-20 15:51:03 -0500

    added things

--- pages/_app.js
+++ pages/_app.js
@@ -37,26 +37,25 @@ async function addIconWallet() {
 function App({ Component, pageProps }) {
   return (
     <div className="pt-4 px-6 mx-auto lg:container flex flex-col">
-    <Navbar  expand="lg" sticky="top">
-      <Container>
-        <Navbar.Brand href="#home"><Image src="/mask_clear.png" width="64" height="64" className="pixelated" alt="Literally Useless Logo" /></Navbar.Brand>
-        <Navbar.Brand className="navbar-brand font-primary">Mask Between Bites and Sips</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#link" className="nav-link hover-underline-animation font-secondary ">Merch Store</Nav.Link>
-            <Nav.Link href="#link" className="nav-link hover-underline-animation font-secondary">Documentation</Nav.Link>
-            <Nav.Link href="#link" className="nav-link hover-underline-animation font-secondary">How It Works</Nav.Link>
-          </Nav>
-          <Navbar.Text className="justify-content-end">
+      <header className="flex items-center justify-between flex-0">
+        <Link href="/">
+          <a className="flex items-center">
+            <Image src="/mask_clear.png" width="64" height="64" className="pixelated" alt="Literally Useless Logo" />
+            <div className="leading-4">
+              <h1 style={{"width": "400px"}} className="font-primary bodyFont leading-5 items-center text-xl tracking-tight text-dark">Mask Between Bites and Sips</h1>
+            </div>
+          </a>
+        </Link>
+        <div className='flex flex-row items-center space-x-8'>
+        <a href="https://google.com" alt="Documentation button" className='font-primary bodyFont2 hover-underline-animation'>Merch Store(coming soon)</a>
+          <a href="https://google.com" alt="Documentation button" className='font-primary bodyFont2 hover-underline-animation'>Documentation</a>
+          <a href="https://google.com" alt="Documentation button" className='font-primary bodyFont2 hover-underline-animation'>How To Buy</a>
+
           <PriceBadge></PriceBadge>
-          </Navbar.Text>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    <Component {...pageProps} /> 
-    <Container fluid='md'>
-    <footer className="w-full left-0 bottom-0 py-4 flex-0">
+        </div>
+      </header>
+      <Component {...pageProps} />
+      <footer className="w-full left-0 bottom-0 py-4 flex-0">
         <div className='w-full flex space-x-4 mb-2 justify-center items-center'>
           <a className='z-10 w-10 h-10 ' href='https://discord.com' target='_blank' rel="noreferrer">
             <Image src="/discord.png" alt="discord logo"></Image>
@@ -65,22 +64,18 @@ function App({ Component, pageProps }) {
             <Image src="/twitter.png" alt="twitter logo"></Image>
           </a>
         </div>
-        <Cards></Cards>   
+        <Cards></Cards>
+        
+        <div className='fixed' style={{"transform": "rotate(20deg)", left: "-420px", bottom:"-420px"}}>
+          <Image src="/pileLeaves.png" alt="pile of trash"></Image>
+        </div>
+        <div className='fixed' style={{"transform": "rotate(-20deg)", right: "-420px", bottom:"-420px"}}>
+          <Image src="/pileLeaves.png" alt="pile of trash"></Image>
+        </div>
       </footer>
-      </Container>  
-      <Container fluid="sm">
-      <div className='fixed sm:text-center' style={{"transform": "rotate(20deg)", left: "-500px", bottom:"-500px"}}>
-          <Container fluid="md">
-          <Image src="/pileLeaves.png" alt="pile of trash"></Image>
-          </Container>
-        </div>
-        <div className='fixed' style={{"transform": "rotate(-20deg)", right: "-500px", bottom:"-500px"}}>
-          <Container fluid="md">
-          <Image src="/pileLeaves.png" alt="pile of trash"></Image>
-          </Container>
-        </div>
-      </Container>
+
     </div>
+    
   )
 }
 
@@ -131,6 +126,54 @@ function App({ Component, pageProps }) {
             </div>
           </div>
         </nav>
+*/
+
+/* This is the code that I tried to make mobile friendly
+<div className="pt-4 px-6 mx-auto lg:container flex flex-col">
+    <Navbar  expand="lg" sticky="top">
+      <Container>
+        <Navbar.Brand href="#home"><Image src="/mask_clear.png" width="64" height="64" className="pixelated" alt="Literally Useless Logo" /></Navbar.Brand>
+        <Navbar.Brand className="navbar-brand font-primary">Mask Between Bites and Sips</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#link" className="nav-link hover-underline-animation font-secondary ">Merch Store</Nav.Link>
+            <Nav.Link href="#link" className="nav-link hover-underline-animation font-secondary">Documentation</Nav.Link>
+            <Nav.Link href="#link" className="nav-link hover-underline-animation font-secondary">How It Works</Nav.Link>
+          </Nav>
+          <Navbar.Text className="justify-content-end">
+          <PriceBadge></PriceBadge>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    <Component {...pageProps} /> 
+    <Container fluid='md'>
+    <footer className="w-full left-0 bottom-0 py-4 flex-0">
+        <div className='w-full flex space-x-4 mb-2 justify-center items-center'>
+          <a className='z-10 w-10 h-10 ' href='https://discord.com' target='_blank' rel="noreferrer">
+            <Image src="/discord.png" alt="discord logo"></Image>
+          </a>
+          <a className='z-10 w-10 h-10 ' href='https://twitter.com/' target='_blank' rel="noreferrer">
+            <Image src="/twitter.png" alt="twitter logo"></Image>
+          </a>
+        </div>
+        <Cards></Cards>   
+      </footer>
+      </Container>  
+      <Container fluid="sm">
+      <div className='fixed sm:text-center' style={{"transform": "rotate(20deg)", left: "-500px", bottom:"-500px"}}>
+          <Container fluid="md">
+          <Image src="/pileLeaves.png" alt="pile of trash"></Image>
+          </Container>
+        </div>
+        <div className='fixed' style={{"transform": "rotate(-20deg)", right: "-500px", bottom:"-500px"}}>
+          <Container fluid="md">
+          <Image src="/pileLeaves.png" alt="pile of trash"></Image>
+          </Container>
+        </div>
+      </Container>
+    </div>
 */
 
 export default App;
